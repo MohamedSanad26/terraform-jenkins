3409b67abb74995f4456876b7c2342cecf4f7061
Mohamed Sanad <mosanad26@gmail.com>
2026-06-28 12:28:25 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0d9858aa3c6322f73"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Jenkins-Terraform"
