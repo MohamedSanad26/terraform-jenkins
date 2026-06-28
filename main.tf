2f81502d782ef42a5d1669890f389e48635ace1b
Mohamed Sanad <mosanad26@gmail.com>
2026-06-28 12:10:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-resource "null_resource" "test" {}
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d9858aa3c6322f73"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform"
+  }
+}
